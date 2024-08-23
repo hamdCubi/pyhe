54f1a60c336bcbf8cb717501ced2931dc2d51482
muhammad Hamd <muhammad.hamd@cubitrek.com>
2024-08-23 12:46:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,3 @@ async def read_root():
     print("yes showing.....")
     return {"message": "Hello, World!"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
